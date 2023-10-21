91613389fe0589082f52877d5fa9d5d909021207
22mikey <140902965+22mikey@users.noreply.github.com>
2023-10-21 19:30:58 +0530

    Add files via upload

---
+++ MIKEY DOM/function.js
@@ -0,0 +1,22 @@
+// function add(a,b){
+//     console.log(a+b)
+//     console.log(a-b)
+//     console.log(a*b)
+//     console.log(a/b)
+// }
+
+// add(16,10)
+
+// function check(n){
+//     if(n%2==0){
+//         console.log("even")
+//     }else{
+//         console.log("odd")
+//     }
+// }
+// check(11)
+// check(3)
+// check(2)
+
+//DOM -> 
+
